9f5a508cef989fd7bae02b349164b12992d98094
Shincode <shahin02@live.se>
2022-08-15 12:36:31 +0200

    performance fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import Question from './components/Question';
 import questions from './questions.json';
 import './App.css';
 
+const getRandomQuestion = () => {
+	const question = questions[Math.floor(Math.random() * questions.length)];
+	return `${question.slice(0, 1).toUpperCase()}${question.slice(1, question.length)}` as never;
+};
+
 export default function App() {
-	const [questionArray, setQuestionArray] = useState([]);
+	const [questionArray, setQuestionArray] = useState([getRandomQuestion()]);
 	const [count, setCount] = useState(1);
 
 	useEffect(() => {
@@ -12,21 +17,8 @@ export default function App() {
 		return () => document.body.removeEventListener('click', moveUp);
 	});
 
-	useEffect(() => {
-		setQuestionArray(() => getRandomQuestions());
-	}, []);
-
-	const getRandomQuestions = () => {
-		return questions
-			.map((question) => `${question.slice(0, 1).toUpperCase()}${question.slice(1, question.length)}`)
-			.sort(() => Math.random() - 0.5) as never[];
-	};
-
 	const moveUp = () => {
-		if (count % questions.length === 0) {
-			setQuestionArray((prev) => [...prev, ...getRandomQuestions()]);
-		}
-
+		setQuestionArray((prev) => [...prev, getRandomQuestion()]);
 		setCount((prev) => prev + 1);
 		(document.querySelector('#question-wrapper') as any).style.bottom = `${500 * count}px`;
 	};
